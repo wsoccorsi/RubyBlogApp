45be8aa0d4f59137b118fbe472bf994a2e30ffda
wsoccorsi <wsoccorsi@gmail.com>
2020-02-10 18:49:34 -0500

    silly error

--- app/controllers/concerns/articles_controller.rb
+++ app/controllers/concerns/articles_controller.rb
@@ -1,5 +1,38 @@
 class ArticlesController < ApplicationController
   def new
+
+    @article = Article.new
+
   end
-  
+
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+
+  def show
+    @article = Article.find(params[:id])
+
+  end
+
+
+  # def index
+  #   @article = Article.find(params[:id])
+  #
+  # end
+  private
+
+  def article_params
+
+    params.require(:article).permit(:title, :description)
+
+  end
+
 end
