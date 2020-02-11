5f25c7308d512ad26dfc93161617d2fab41e699d
wsoccorsi <wsoccorsi@gmail.com>
2020-02-11 13:14:48 -0500

    finished crud and got rid of redundancy

--- app/controllers/concerns/articles_controller.rb
+++ app/controllers/concerns/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+
   def new
 
     @article = Article.new
@@ -23,16 +26,23 @@ class ArticlesController < ApplicationController
 
 
   def show
-    @article = Article.find(params[:id])
+    # @article = Article.find(params[:id])
 
+  end
+
+  def destroy
+    # @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "article was successfully deleted"
+    redirect_to articles_path
   end
 
   def edit
-    @article = Article.find(params[:id])
+    # @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
+    # @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
       redirect_to article_path(@article)
@@ -42,6 +52,10 @@ class ArticlesController < ApplicationController
   end
 
   private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
   def article_params
 
