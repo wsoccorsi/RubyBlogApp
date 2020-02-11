872c7c9b81e44eb02666417f1eae9fd486670a69
wsoccorsi <wsoccorsi@gmail.com>
2020-02-10 19:31:45 -0500

    editing stuff

--- app/controllers/concerns/articles_controller.rb
+++ app/controllers/concerns/articles_controller.rb
@@ -22,11 +22,20 @@ class ArticlesController < ApplicationController
 
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
 
-  # def index
-  #   @article = Article.find(params[:id])
-  #
-  # end
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def article_params
